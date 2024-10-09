5dbfd3eb7817f0be3042d487acd3e3f452759b7a
devAn <andretdswork@gmail.com>
2024-10-09 00:15:30 -0300

    Ajustes gerais

--- 01-create-database/08-classificacao-contabil-sap.sql
+++ 01-create-database/08-classificacao-contabil-sap.sql
@@ -4,3 +4,9 @@ CREATE TABLE CLASSIF_CONTABIL_SAP(
     NOME_LIMPO VARCHAR2(40),
     DTCRIACAO DATE DEFAULT SYSDATE
 );
+
+INSERT INTO classif_contabil_sap VALUES (1,'Provisao','PROVISAODEMANUTENCAO',sysdate);
+INSERT INTO classif_contabil_sap VALUES (2,'Intangivel','INTANGIVEL',sysdate);
+INSERT INTO classif_contabil_sap VALUES (3,'Imobilizado','IMOBILIZADO',sysdate);
+INSERT INTO classif_contabil_sap VALUES (4,'Sofware','SOFTWARE',sysdate);
+COMMIT;
